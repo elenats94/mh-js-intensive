02c12fc89f51db35622ffeb84fa1da415fe8bcf8
Elena Tsvetkova <git@etdev.space>
2023-02-11 11:15:09 +0300

    lecture 02

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-const skill = document.getElementById('skill');
-const isLove = document.getElementById('is_love');
+// STRING
 
-const skillText = prompt('Какой язык вы учите?', 'пока не в курсе');
-const isLoveValue = confirm('Вы любите изучаемый язык?');
+// const str1 = 'I\'m okay';
+// const str2 = "Say:\n\"hello\"";
+// const str3 = `I'm here
+// and I say: "${str1}!"`;
+//
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
 
-skill.innerText = skillText;
-if (isLoveValue) {
-    isLove.innerText = 'да';
-} else {
-    isLove.innerText = 'нет';
-}
+
+// NUMBER
+
+// const num1 = 75 + 15;
+// const num2 = 79 * 9;
+
+// NaN
+// const inf = 17 / 0
+
+// BIGINT (add n to the end, numbers from -2^-53-1 to 2^53-1)
+
+// const bigint = 10312312547839273345443253n;
+// console.log(`number=${bigint}, type: ${typeof(bigint)}`);
+
+// BOOLEAN - true / false
+
+// const bool = "a" > "AAA";
+// console.log(bool);
+
+// NULL
+// const empty = null;
+// console.log(empty, typeof empty);
+
+// UNDEFINED
+
+// let box;
+// console.log(box, typeof box);
+
+// SYMBOL
+// const uniq = Symbol('id');
+// const uniq2 = Symbol('id');
+// console.log(uniq === uniq2);
+
+// OBJECT
+// const obj = {
+//     name: 'Sasha',
+//     age: 19,
+//     isHappy: true,
+// }
+
+// console.log(obj.name);
+// console.log(obj['age']);
+
+// obj.job = 'Google';
+// console.log(obj)
+
+// const arr = ['Anna', 18, false];
+// arr[3] = "Facebook";
+// console.log(arr);
+// console.log(arr['job']);
